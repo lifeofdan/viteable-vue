7dd1cb8c13144100dd2802cb69f600df56788a72
Daniel Newman <contact@lifeofdan.me>
2022-07-18 01:55:04 +0000

    Basic implementation of headless button and input

---
+++ src/components/headless/buttons/xbutton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button style="display: flex" type="button">
+      <slot name="leftIcon" />
+      <slot />
+      <slot name="rightIcon" />
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label?: string,
+}
+withDefaults(defineProps<Props>(), {
+  label: ''
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/headless/inputs/xinput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label :class="labelClasses" :style="`text-align: left; ${labelStyles}`">
+      {{ label }}
+      <div style="display: flex">
+        <slot name="leftIcon" />
+          <input :class="inputClasses" :style="inputStyles" :type="type" />
+        <slot name="rightIcon" />
+      </div>
+      
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: string,
+  label?: string,
+  labelClasses?: string,
+  labelStyles?: string,
+  inputClasses?: string,
+  inputStyles?: string,
+}
+withDefaults(defineProps<Props>(), {
+  inputClasses: '',
+  type: 'text',
+  label: '',
+  labelClasses: '',
+  labelStyles: ''
+})
+</script>
+
+<style scoped>
+
+</style>
